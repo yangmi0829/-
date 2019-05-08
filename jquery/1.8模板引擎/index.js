ddfeea372c5d7e6bca3cd88a524dca198e388852
yangmi <33408426+yangmi0829@users.noreply.github.com>
2019-05-08 20:09:34 +0800

    Create index.js

---
+++ jquery/1.8模板引擎/index.js
@@ -0,0 +1,30 @@
+/**
+ * <%=%> 常量
+ * <%-%> 特殊字符
+ * <% %> js代码
+ *
+ * @type {string}
+ */
+
+const setting = {
+  constants: /<%=\s*(.*?)\s*%>/,
+  char: /<%-\s*(.*?)\s*%>/,
+  code: /<%\s*(.*?)\s*%>/,
+}
+var regex = new RegExp(Object.values(setting).map(item => item.source).join("|"),'g')
+function template (str) {
+  return complier.bind(this,str)
+}
+
+function complier (str, data) {
+  return str.replace(regex,function (match,constants,char,code) {
+    if(constants){
+      return data[constants]
+    }else if(code){
+      eval(code)
+      return ''
+    }else if(char){
+      return data[char]
+    }
+  })
+}

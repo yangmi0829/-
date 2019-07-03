7811306a8bad82c2558327bc056694ae37c82bb4
yangmi <33408426+yangmi0829@users.noreply.github.com>
2019-07-03 18:09:07 +0800

    Create regex.js

---
+++ exam/chapter/regex.js
@@ -0,0 +1,8 @@
+// 密码正则 6-12位  由数字大小写字母组成，至少两种字符
+// 钱正则 3位加逗号
+
+const password_regex = /^(?!\d{6,12}$)(?![a-zA-Z]{6,12}$)(\w{6,12}$)/g
+const money_regex = /\B(?=(?:\d{3})+(?!\d))/g
+console.log(password_regex.test("012345678910"))
+
+console.log("12345678900".replace(money_regex,",") )
